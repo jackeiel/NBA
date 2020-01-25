0fca942d89d0bbaf953d772c11b1f7f3d530581c
jackeiel <jackneiel@gmail.com>
2020-01-25 15:39:20 -0500

    added threshold for todays bets

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 *Project in progress*   
 *Updates coming soon*   
 
-Built off of swar's nba_api ([https://github.com/swar/nba_api])
+Built off of swar's nba_api ([1])
 
 
 **Usage**  
@@ -21,16 +21,10 @@ Output is in terms of the home team (i.e. prediction is DEN wins by 2.3)
 1. Command Line Interface
 2. Back test input generation method   
     Currently using rolling average from last 6 games
-3. Scrape daily lines
-4. Write bet algo
-5. Method for recording bets and results
-    
-    
-    
-    
-    
-    
-    
-    s
+3. ~~Scrape daily lines~~
+4. ~~Write bet algo~~
+5. Method for recording bets and results  
 
-[]: https://github.com/swar/nba_api
+    
+
+[1]: https://github.com/swar/nba_api
--- scripts/scripts.py
+++ scripts/scripts.py
@@ -8,7 +8,15 @@ from utilities import find_games, get_lines
 pd.set_option('display.width', 1500)
 pd.set_option('display.max_columns', 100)
 
-def todays_bets(book='Caesars', display=True, save=True):
+def todays_bets(book='Caesars', threshold=4, display=True, save=True):
+    """
+    Generates potential value bets for todays games.
+    :param book (str, optional): The sports book to use
+    :param threshold (int, optional): The value threshold for making a bet
+    :param display (bool, optional): Print resulting DataFrame to screen
+    :param save (bool, optional): Save csv file to DATA/Predictions folder
+    :return: DataFrame of games and bets
+    """
     lines = get_lines(sportsbook=book)
     games = find_games()
 
@@ -16,15 +24,15 @@ def todays_bets(book='Caesars', display=True, save=True):
     full = games.merge(lines, how='left', left_on=['HOME_TEAM_ID', 'VISITOR_TEAM_ID'],
                        right_on=['home_team', 'away_team'])
 
-    def internal_predict(row): # must be abbrevs
+    def internal_predict(row):
         sleep(10)
         return GamePredict(row.home_team, row.away_team).predict_spread()
 
     full['predicted_spread'] = full.apply(internal_predict, axis=1)
 
     #TODO write bet algo
-    def place_bet(row):
-        if max(row.vegas_line_home, row.predicted_spread) - min(row.vegas_line_home, row.predicted_spread) > 4:
+    def place_bet(row, threshold=4):
+        if max(row.vegas_line_home, row.predicted_spread) - min(row.vegas_line_home, row.predicted_spread) > threshold:
             if row.vegas_line_home - row.predicted_spread > row.predicted_spread - row.vegas_line_home:
                 return row.home_team
             else:
@@ -32,7 +40,7 @@ def todays_bets(book='Caesars', display=True, save=True):
         else:
             return '---'
 
-    full['bet'] = full.apply(place_bet, axis=1)
+    full['bet'] = full.apply(place_bet, threshold=threshold, axis=1)
 
     # full = full.drop('GAME_DATE_EST', axis=1)
 
@@ -43,3 +51,5 @@ def todays_bets(book='Caesars', display=True, save=True):
         # TODO simple date
         date = pd.Timestamp('today').date()
         full.to_csv(f'DATA/Predictions/{date}.csv')
+
+    return full
